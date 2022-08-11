73afa4bcc879b72458856c56815059c26516bc81
mmvergara <royaleygaming@gmail.com>
2022-08-11 09:55:22 +0800

    new projects

---
+++ 015 Project - Tailwind Practice/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container px-4 text-center'>
+        <p className='sm:font-bold  sm:text-red-300 md:text-green-300 lg:text-cyan-300'>asadasdasdasd</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
