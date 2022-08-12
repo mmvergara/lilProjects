71171a8d61050cb86171187a4d41eada20e47373
mmvergara <royaleygaming@gmail.com>
2022-08-12 15:10:59 +0800

    practicing tailwind

--- 014 Project - Tailwind Practice/src/App.tsx
+++ 014 Project - Tailwind Practice/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+
         <p className='text-xl sm:font-bold text-white inline pl-5 pr-2 bg-gradient-to-r  to-blue-500 from-black box-decoration-clone'>
-          Vergara <br /> 
-          Mark Matthew <br /> 
-          M.
+          This is a Sample Text<br />
+          This is the second Sample <br />
           </p>
     </div>
   );
