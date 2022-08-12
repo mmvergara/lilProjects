62832ea191f7a6dc6a26728e9657e4248c29a360
mmvergara <royaleygaming@gmail.com>
2022-08-12 14:42:40 +0800

    practicing tailwind

--- 014 Project - Tailwind Practice/tailwind.config.js
+++ 014 Project - Tailwind Practice/tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
   // purge: ['.scr/**/*.{js,jsx,ts,tsx}','./public/index.html'],
   theme: {
     extend: {},
+    screens: {
+      'xsm': '320px',
+      // => @media (min-width: 640px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      'fullHD': '1920px',
+      // => @media (min-width: 640px) { ... }
+    },
   },
   plugins: [],
 }
