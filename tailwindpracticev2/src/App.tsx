7c4c7e97ffce3ede7ff86419c97757636714f558
mmvergara <royaleygaming@gmail.com>
2022-08-13 12:55:46 +0800

    updating tailwindPractice

--- tailwindpracticev2/src/App.tsx
+++ tailwindpracticev2/src/App.tsx
@@ -1,8 +1,7 @@
+
 import './App.css';
 import SideBar from './components/SideBar';
 
-
-const logo = require('./logo.svg') as string
 function App() {
 
   return (
@@ -13,12 +12,10 @@ function App() {
         <h1 className='text-3xl font-bold text-center blur'>Blurry</h1>
         <h1 className='text-3xl font-bold text-center mix-blend-soft-light'>Soft Light</h1>
         <h1 className='text-3xl font-bold text-center ring-4 my-2 ring-red-500 '>Ring</h1>
-        <h1 className='text-3xl font-bold text-center ring-4 ring-opacity-60 my-2 ring-red-500 '>Ring</h1>
-        <img src={logo} alt="sad" />
       </div>
-      
     </div>
   );
 }
 
 export default App;
+
