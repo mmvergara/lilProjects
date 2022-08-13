0fec1a5c42f90ef97950f5f689361eec3e934f1a
mmvergara <royaleygaming@gmail.com>
2022-08-13 17:16:59 +0800

    added updates on TICTACTOE

--- 013 Game - TicTacToe/script.js
+++ 013 Game - TicTacToe/script.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function autoBind(_, _2, propertyDescriptor) {
     const origConstructor = propertyDescriptor.value;
     const adjConstructor = {
@@ -11,3 +17,137 @@ function autoBind(_, _2, propertyDescriptor) {
     };
     return adjConstructor;
 }
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
+// horizontal winning conditions
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
+// vertical winning conditions
+// [1,4,7]
+// [2,5,8]
+// [7,8,9]
+// diagonal winning conditions
+// [1,5,9]
+// [7,5,3]
+class TicTacToe {
+    constructor() {
+        this.winningConditions = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [7, 8, 9],
+            [1, 5, 9],
+            [7, 5, 3],
+        ];
+        this.playerXstatus = [];
+        this.playerOstatus = [];
+        this.clickedBoxes = [];
+        this.CurrentPlayer = 'O';
+        this.tileContainer = document.querySelector('.tileContainer');
+        this.winnertText = document.querySelector('.Winner');
+        this.getLocalStorage();
+        this.reRenderXnO();
+        this.tileContainer.addEventListener('click', this.clickHandler);
+    }
+    clickHandler(e) {
+        console.log(this.CurrentPlayer);
+        const targetBox = e.target;
+        const tileID = Number(targetBox.getAttribute('data-tileid'));
+        // guard clause for clicking the same
+        if (this.clickedBoxes.includes(tileID))
+            return;
+        targetBox.textContent = this.CurrentPlayer;
+        // make the box not clickable - update clickedboxes array
+        targetBox.style.cursor = 'not-allowed';
+        this.clickedBoxes.push(tileID);
+        // update playersArray - reverse current player 
+        if (this.CurrentPlayer == 'O') {
+            this.playerOstatus.push(tileID);
+            console.log('O status -' + this.playerOstatus);
+            targetBox.style.color = '#ff0000ce';
+            // Check if O won
+            this.CurrentPlayer = 'X';
+        }
+        else {
+            this.playerXstatus.push(Number(tileID));
+            console.log('X status -' + this.playerOstatus);
+            targetBox.style.color = '#0091ffce';
+            // Check if O won
+            this.CurrentPlayer = 'O';
+        }
+        // Check Status
+        this.checkStatus();
+        this.setLocalStorage();
+    }
+    checkStatus() {
+        if (this.winningConditions.some(y => y.every(x => this.playerOstatus.includes(x)))) {
+            // player O won
+            console.log('player O won');
+            this.winnertText.innerHTML = 'Player O won!!! <br/> Game reset!...';
+            this.resetGame();
+        }
+        if (this.winningConditions.some(y => y.every(x => this.playerXstatus.includes(x)))) {
+            // player X won
+            console.log('player X won');
+            this.winnertText.innerHTML = 'Player X won!!! <br/> Game reset!...';
+            this.resetGame();
+        }
+        if ((this.playerOstatus.length + this.playerXstatus.length) == 9) {
+            console.log('Draw');
+            this.winnertText.innerHTML = 'Draw!!! <br/> Game reset!...';
+            this.resetGame();
+        }
+    }
+    resetGame() {
+        this.playerXstatus = [];
+        this.playerOstatus = [];
+        this.clickedBoxes = [];
+        this.CurrentPlayer = 'O';
+        this.tileContainer.querySelectorAll('.tile').forEach((x) => {
+            x.textContent = '';
+            x.style.cursor = 'pointer';
+        });
+        setTimeout(() => {
+            this.winnertText.innerHTML = '';
+        }, 1000);
+    }
+    setLocalStorage() {
+        const gameStatus = {
+            pX: this.playerXstatus,
+            pO: this.playerOstatus,
+            cB: this.clickedBoxes,
+            cP: this.CurrentPlayer
+        };
+        localStorage.setItem('gameStatus', JSON.stringify(gameStatus));
+    }
+    getLocalStorage() {
+        const retrieved = JSON.parse(localStorage.getItem('gameStatus'));
+        const { pX, pO, cB, cP } = retrieved;
+        if (!retrieved)
+            return;
+        this.playerXstatus = pX;
+        this.playerOstatus = pO;
+        this.clickedBoxes = cB;
+        this.CurrentPlayer = cP;
+    }
+    reRenderXnO() {
+        this.playerOstatus.forEach((x) => {
+            const tile = document.querySelector(`[data-tileid="${x}"]`);
+            tile.textContent = 'O';
+            tile.style.color = '#ff0000ce';
+        });
+        this.playerXstatus.forEach((x) => {
+            const tile = document.querySelector(`[data-tileid="${x}"]`);
+            tile.textContent = 'X';
+            tile.style.color = '#0091ffce';
+        });
+    }
+}
+__decorate([
+    autoBind
+], TicTacToe.prototype, "clickHandler", null);
+const firstTick = new TicTacToe();
--- 013 Game - TicTacToe/script.ts
+++ 013 Game - TicTacToe/script.ts
@@ -1,4 +1,3 @@
-
 function autoBind(_:any,_2:any,propertyDescriptor:PropertyDescriptor){
     const origConstructor = propertyDescriptor.value
     const adjConstructor = {
@@ -11,3 +10,170 @@ function autoBind(_:any,_2:any,propertyDescriptor:PropertyDescriptor){
     }
     return adjConstructor
 }
+
+
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
+
+// horizontal winning conditions
+// [1,2,3]
+// [4,5,6]
+// [7,8,9]
+
+// vertical winning conditions
+// [1,4,7]
+// [2,5,8]
+// [7,8,9]
+
+// diagonal winning conditions
+// [1,5,9]
+// [7,5,3]
+
+
+class TicTacToe {
+
+    tileContainer:HTMLElement;
+    winnertText:HTMLElement;
+    private winningConditions :[number,number,number][]= [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+
+        [1,4,7],
+        [2,5,8],
+        [7,8,9],
+
+        [1,5,9],
+        [7,5,3],
+    ]
+
+    playerXstatus:number[] = []
+    playerOstatus:number[] = []
+    clickedBoxes:number[] = []
+    CurrentPlayer:'X'|'O' = 'O'
+
+    constructor() {
+        this.tileContainer = document.querySelector('.tileContainer')! as HTMLElement
+        this.winnertText = document.querySelector('.Winner')! as HTMLElement
+        this.getLocalStorage()
+        this.reRenderXnO()
+        this.tileContainer.addEventListener('click',this.clickHandler)
+    
+    }
+
+    @autoBind
+    private clickHandler(e:Event) {
+        console.log(this.CurrentPlayer)
+
+        const targetBox = e.target! as HTMLElement
+        const tileID = Number(targetBox.getAttribute('data-tileid'))
+
+        // guard clause for clicking the same
+        if(this.clickedBoxes.includes(tileID)) return
+        targetBox.textContent = this.CurrentPlayer
+
+        // make the box not clickable - update clickedboxes array
+        targetBox.style.cursor = 'not-allowed'
+        this.clickedBoxes.push(tileID)
+
+        // update playersArray - reverse current player 
+        if(this.CurrentPlayer == 'O'){
+            this.playerOstatus.push(tileID)
+            console.log('O status -'+this.playerOstatus)
+            targetBox.style.color = '#ff0000ce'
+            
+            // Check if O won
+            this.CurrentPlayer = 'X'
+        } else {
+            this.playerXstatus.push(Number(tileID))
+            console.log('X status -'+this.playerOstatus)
+            targetBox.style.color = '#0091ffce'
+
+            // Check if O won
+            this.CurrentPlayer = 'O'
+        }
+
+        // Check Status
+        this.checkStatus()
+        this.setLocalStorage()
+
+    }
+
+    private checkStatus() {
+        if(this.winningConditions.some(y=> y.every(x=> this.playerOstatus.includes(x)))){
+            // player O won
+            console.log('player O won')
+            this.winnertText.innerHTML = 'Player O won!!! <br/> Game reset!...'
+            this.resetGame()
+        }
+
+        if(this.winningConditions.some(y=> y.every(x=> this.playerXstatus.includes(x)))){
+            // player X won
+            console.log('player X won')
+            this.winnertText.innerHTML = 'Player X won!!! <br/> Game reset!...'
+            this.resetGame()
+        }
+
+        if((this.playerOstatus.length + this.playerXstatus.length) == 9) {
+            console.log('Draw')
+            this.winnertText.innerHTML = 'Draw!!! <br/> Game reset!...'
+            this.resetGame()
+        }
+
+    }
+
+    private resetGame() {
+        this.playerXstatus = []
+        this.playerOstatus = []
+        this.clickedBoxes = []
+        this.CurrentPlayer = 'O'
+        
+        this.tileContainer.querySelectorAll('.tile').forEach((x:any)=>{
+            x.textContent = '';
+            x.style.cursor = 'pointer'
+        })
+
+        setTimeout(()=>{
+            this.winnertText.innerHTML = ''
+        },1000)
+    }
+
+    private setLocalStorage () {
+        const gameStatus = {
+            pX:this.playerXstatus,
+            pO:this.playerOstatus,
+            cB:this.clickedBoxes,
+            cP:this.CurrentPlayer
+        }
+        localStorage.setItem('gameStatus',JSON.stringify(gameStatus))
+    }
+
+    private getLocalStorage () {
+        const retrieved = JSON.parse(localStorage.getItem('gameStatus')!)
+        const {pX,pO,cB,cP} = retrieved
+        if(!retrieved) return
+            this.playerXstatus = pX
+            this.playerOstatus = pO
+            this.clickedBoxes = cB
+            this.CurrentPlayer = cP
+    }
+
+    private reRenderXnO () {
+        this.playerOstatus.forEach((x:number)=>{
+            const tile = document.querySelector(`[data-tileid="${x}"]`)! as HTMLElement
+            tile.textContent = 'O'
+            tile.style.color = '#ff0000ce'
+        })
+
+        this.playerXstatus.forEach((x:number)=>{
+            const tile = document.querySelector(`[data-tileid="${x}"]`)! as HTMLElement
+            tile.textContent = 'X'
+            tile.style.color = '#0091ffce'
+        })
+
+    }
+
+}
+
+const firstTick = new TicTacToe()
